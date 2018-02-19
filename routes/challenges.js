cb98143a50fa2ccce77e896188d8eecf04af8c2b
kmh11 <hoppykev@gmail.com>
2018-02-19 16:24:42 -0500

    flag verification

--- routes/challenges.js
+++ routes/challenges.js
@@ -4,12 +4,33 @@ var User = require('../models/user')
 var Team = require('../models/team')
 var Challenge = require('../models/challenge')
 var responses = require('../responses')
+var crypto = require('crypto')
 var router = express.Router()
 
+// get list of challenges
 router.get('/', passport.authenticate('jwt', { session: false }), function (req, res) {
-  Challenge.find({}, function (err, challenges) {
-    res.json(challenges.map(challenge => responses.challenge(challenge)))
-  })
+  if (req.user.team) {
+    Challenge.find({}, function (err, challenges) {
+      res.json(challenges.map(challenge => responses.challenge(challenge)))
+    })
+  }
+})
+
+// submit flag
+router.post('/:id/submissions', passport.authenticate('jwt', { session: false }), function (req, res) {
+  if (req.user.team) {
+    Challenge.findOne({_id: req.params.id}, function (err, challenge) {
+      if (err) {
+        res.sendStatus(404)
+      } else {
+        if (crypto.createHash('sha512').update(req.body.flag).digest('hex') === challenge.flag) {
+          res.json({correct: true})
+        } else {
+          res.json({correct: false})
+        }
+      }
+    })
+  }
 })
 
 module.exports = router
