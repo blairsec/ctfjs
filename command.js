f2be6662c66e2e230dbe87b53e1505be58a1ff7b
Kevin Higgs <github@kevinhiggs.com>
2018-10-13 15:06:44 -0400

    environment variables in command

--- command.js
+++ command.js
@@ -8,11 +8,13 @@ var path = require('path')
 var config = {
   jwt_secret: crypto.randomBytes(64).toString('hex'),
   db_uri: 'postgresql://ctf@localhost:5432/ctf',
-  port: 3000
+  port: 3000,
+  env: {}
 }
 
 function start (callback) {
   var env = { PORT: config.port, DATABASE_URI: config.db_uri, SECRET_KEY: config.jwt_secret, CORS_ORIGIN: config.cors_origin }
+  Object.assign(env, config.env)
   if (config.plugin_folder) env.PLUGIN_FOLDER = config.plugin_folder
   pm2.start(path.join(__dirname, 'index.js'), { env: env, name: 'ctf' }, function (err, proc) {
     if (err && err.message === 'Script already launched') console.log('Error: ctfjs already running')
@@ -36,12 +38,21 @@ program
   .option('-p, --port <port>', 'port to listen on')
   .option('-o, --cors-origin <cors_origin>', 'comma separated list of origins for CORS header')
   .option('-f, --plugin-folder <plugin_folder>', 'plugins folder')
+  .option('-e, --environment <environment_var>s', 'environment variables in the form <key>=<val> separated by semicolons')
   .action(function (command) {
     if (command.secretKey) config.jwt_secret = command.secretKey
     if (command.database) config.db_uri = command.database
     if (command.port) config.port = command.port
     if (command.pluginFolder) config.plugin_folder = path.resolve(command.pluginFolder)
     if (command.corsOrigin) config.cors_origin = command.corsOrigin
+    if (command.environment) {
+      command.environment = command.environment.split(";")
+      for (var i = 0; i < command.environment.length; i++) {
+        var env = command.environment[i].split(/^([^=]+)=/)
+        env.splice(0, 1)
+        config.env[env[0]] = env[1]
+      }
+    }
     if (command.restart) stop(function () { start(function () { process.exit(0) }) })
     else start(function () { process.exit(0) })
   })
